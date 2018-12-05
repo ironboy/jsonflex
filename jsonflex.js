6a6f125dd744474e4890d73f73a85a0b0f41efde
Kristoffer <doedmannen@aetmej.se>
2018-12-05 16:02:47 +0100

    Fixed bug

--- jsonflex.js
+++ jsonflex.js
@@ -83,6 +83,7 @@
   }
 
   JSON._stringify = (obj, replacer, space = '  ')=>{
+    obj = Array.isArray(obj) ? obj : [obj];
     recurser(obj);
     let result = JSON.stringify(obj, replacer, space);
     recurser(obj, true);
@@ -92,6 +93,7 @@
   JSON._parse = (str, reviver) => {
     let obj = JSON.parse(str, reviver);
     recurser(obj, 'hard');
+    obj = Array.isArray(obj) && obj.length === 1 ? obj[0] : obj;
     return obj;
   }
 
